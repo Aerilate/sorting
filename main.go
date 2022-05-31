3f58ea886a1334250e86e276facb0b2d052e0f48
Raymond Wang <raymondzwng@gmail.com>
2022-05-30 20:18:32 -0400

    Added benchmarking

--- main.go
+++ main.go
@@ -3,11 +3,39 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
 
 	"github.com/Aerilate/sorting/sort"
 )
 
-const SIZE = 10
+const SIZE = 1000000
+
+func main() {
+	var sorted []int
+	for i := 0; i < SIZE; i++ {
+		sorted = append(sorted, i)
+	}
+
+	type sortingAlgo func ([]int)
+	algos := []sortingAlgo{
+		sort.Heapsort, 
+		sort.Mergesort, 
+		sort.Quicksort,
+	}
+
+	fmt.Printf("%-12v %-10v %v\n\n", "Algorithm", "Time (ms)", "Error")
+	for _, algo := range algos {
+		unsorted := make([]int, len(sorted))
+		copy(unsorted, sorted)
+		Shuffle(unsorted)
+
+		elapsed, err := benchmark(algo, unsorted)
+		fmt.Printf("%-12v %-10v %v\n", getFuncName(algo), elapsed, err)
+	}
+}
 
 // Fisher-Yates shuffle
 func Shuffle(nums []int) {
@@ -17,7 +45,7 @@ func Shuffle(nums []int) {
 	}
 }
 
-func isSorted(nums []int) bool {
+func validated(nums []int) bool {
 	for idx, val := range nums {
 		if idx != val {
 			return false
@@ -26,18 +54,18 @@ func isSorted(nums []int) bool {
 	return true
 }
 
-func main() {
-	var sorted []int
-	for i := 0; i < SIZE; i++ {
-		sorted = append(sorted, i)
+func benchmark(sort func ([]int), input []int) (elapsed int64, err error) {
+	start := time.Now()
+	sort(input)
+	elapsed = time.Since(start).Milliseconds()
+
+	if !validated(input) {
+		return elapsed, fmt.Errorf("algorithm did not sort correctly")
 	}
+	return elapsed, nil
+}
 
-	unsorted := make([]int, len(sorted))
-	copy(unsorted, sorted)
-	Shuffle(unsorted)
-
-	sort.Heapsort(unsorted)
-
-	fmt.Println(unsorted)
-	fmt.Println(sorted)
+func getFuncName(i interface{}) string {
+	name := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+    return strings.Split(name, ".")[2]
 }
